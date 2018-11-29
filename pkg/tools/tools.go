e0bf4370782ed513145f229c7e4ea221dd7d4041
Amandine Drebes <amandine@energie-ip.com>
2018-11-29 09:01:28 +0100

    Initial Commit

---
+++ pkg/tools/tools.go
@@ -0,0 +1,43 @@
+package tools
+
+import (
+	"bytes"
+	"net"
+	"strings"
+)
+
+//GetNetworkInfo return current ip and mac address
+func GetNetworkInfo() (addr, ip string) {
+	interfaces, err := net.Interfaces()
+	if err == nil {
+		for _, iface := range interfaces {
+			if iface.Flags&net.FlagUp != 0 && bytes.Compare(iface.HardwareAddr, nil) != 0 {
+				addr = iface.HardwareAddr.String()
+				ips, err := iface.Addrs()
+				if err != nil {
+					return
+				}
+				if len(ips) > 0 {
+					ipValue := ips[0].String()
+					ip = strings.Split(ipValue, "/")[0]
+				}
+				break
+			}
+		}
+	}
+	return
+}
+
+//GetMac return current mac address
+func GetMac() (addr string) {
+	interfaces, err := net.Interfaces()
+	if err == nil {
+		for _, i := range interfaces {
+			if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
+				addr = i.HardwareAddr.String()
+				break
+			}
+		}
+	}
+	return
+}
